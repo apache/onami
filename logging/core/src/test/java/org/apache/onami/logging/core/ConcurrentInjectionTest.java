7e3310be780320eb7361911dda461b72c8736ce7
Simone Tripodi <simonetripodi@apache.org>
2013-01-20 20:20:00 +0000

    code format, no functional modifications
    git-svn-id: https://svn.apache.org/repos/asf/incubator/onami/trunk@1435941 13f79535-47bb-0310-9956-ffa450edef68

--- logging/core/src/test/java/org/apache/onami/logging/core/ConcurrentInjectionTest.java
+++ logging/core/src/test/java/org/apache/onami/logging/core/ConcurrentInjectionTest.java
@@ -1,5 +1,6 @@
 package org.apache.onami.logging.core;
 
+import static com.google.inject.Guice.createInjector;
 import static org.testng.Assert.assertNotNull;
 
 import java.lang.reflect.Field;
@@ -7,7 +8,6 @@ import java.lang.reflect.Field;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.TypeLiteral;
 import com.google.inject.matcher.Matchers;
@@ -16,40 +16,53 @@ public class ConcurrentInjectionTest {
     private Injector injector;
 
     @BeforeTest
-    public void setUp() {
-        injector = Guice.createInjector(
-                new AbstractLoggingModule<Object>(Matchers.only(TypeLiteral.get(InjectionTarget.class)), TestLoggerInjector.class)
-        );
+    public void setUp()
+    {
+        injector = createInjector( new AbstractLoggingModule<Object>( Matchers.only( TypeLiteral.get( InjectionTarget.class ) ),
+                                                                      TestLoggerInjector.class ) );
     }
 
-    @Test(threadPoolSize = 25, invocationCount = 5000)
-    public void injectAndVerify() {
+    @Test( threadPoolSize = 25, invocationCount = 5000 )
+    public void injectAndVerify()
+    {
         InjectionTarget target = new InjectionTarget();
-        injector.injectMembers(target);
-        assertNotNull(target.logger);
+        injector.injectMembers( target );
+        assertNotNull( target.logger );
     }
 
-    private static class TestLoggerInjector extends AbstractLoggerInjector<Object> {
+    private static class TestLoggerInjector
+        extends AbstractLoggerInjector<Object>
+    {
+
         private volatile Object barrier;
 
-        public TestLoggerInjector(Field field) {
-            super(field);
+        public TestLoggerInjector( Field field )
+        {
+            super( field );
         }
 
         @Override
-        protected Object createLogger(Class<?> clazz) {
+        protected Object createLogger( Class<?> clazz )
+        {
             Object result = new Object();
             barrier = result; // artificially trigger a memory barrier
-            try {
-                Thread.sleep(20);
-            } catch (InterruptedException e) {
+            try
+            {
+                Thread.sleep( 20 );
+            }
+            catch ( InterruptedException e )
+            {
             }
             return result;
         }
     }
 
-    private static class InjectionTarget {
+    private static class InjectionTarget
+    {
+
         @InjectLogger
         private Object logger;
+
     }
+
 }
