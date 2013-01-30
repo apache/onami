3aebf7676be25479e0950266d77b29b9e3e361ee
Simone Tripodi <simonetripodi@apache.org>
2013-01-30 20:48:25 +0000

    [ONAMI-72] #comment code format aligned to Onami conventions #resolve
    git-svn-id: https://svn.apache.org/repos/asf/incubator/onami/sandbox/validation@1440659 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/onami/validation/GuiceAwareConstraintValidatorFactory.java
+++ src/main/java/org/apache/onami/validation/GuiceAwareConstraintValidatorFactory.java
@@ -1,20 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.onami.validation;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -24,26 +27,27 @@ import javax.validation.ConstraintValidatorFactory;
 import com.google.inject.Injector;
 
 /**
- * {@code javax.validation.ConstraintValidatorFactory} implementation that
- * relies on Google Guice.
- *
- * @version $Id$
+ * {@code javax.validation.ConstraintValidatorFactory} implementation that relies on Google Guice.
  */
 @Singleton
-final class GuiceAwareConstraintValidatorFactory implements ConstraintValidatorFactory {
+final class GuiceAwareConstraintValidatorFactory
+    implements ConstraintValidatorFactory
+{
 
     @Inject
     private Injector injector;
 
-    public void setInjector(Injector injector) {
+    public void setInjector( Injector injector )
+    {
         this.injector = injector;
     }
 
     /**
      * {@inheritDoc}
      */
-    public <T extends ConstraintValidator<?, ?>> T getInstance(Class<T> key) {
-        return this.injector.getInstance(key);
+    public <T extends ConstraintValidator<?, ?>> T getInstance( Class<T> key )
+    {
+        return this.injector.getInstance( key );
     }
 
 }
--- src/main/java/org/apache/onami/validation/Validate.java
+++ src/main/java/org/apache/onami/validation/Validate.java
@@ -1,20 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.onami.validation;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -27,13 +30,12 @@ import com.google.inject.BindingAnnotation;
 
 /**
  * Marker for methods which arguments have to be validated.
- *
- * @version $Id$
  */
 @BindingAnnotation
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD })
-public @interface Validate {
+@Retention( RetentionPolicy.RUNTIME )
+@Target( { ElementType.METHOD } )
+public @interface Validate
+{
 
     /**
      * The groups have to be validated, empty by default.
@@ -43,8 +45,7 @@ public @interface Validate {
     Class<?>[] groups() default {};
 
     /**
-     * Marks if the returned object by the intercepted method execution has to
-     * be validated, false by default.
+     * Marks if the returned object by the intercepted method execution has to be validated, false by default.
      *
      * @return false by default.
      */
@@ -53,16 +54,13 @@ public @interface Validate {
     /**
      * The exception re-thrown when an error occurs during the validation.
      *
-     * @return the exception re-thrown when an error occurs during the
-     *         validation.
+     * @return the exception re-thrown when an error occurs during the validation.
      */
     Class<? extends Throwable> rethrowExceptionsAs() default ConstraintViolationException.class;
 
     /**
-     * A custom error message when throwing the custom exception.
-     *
-     * It supports java.util.Formatter place holders, intercepted method
-     * arguments will be used as message format arguments.
+     * A custom error message when throwing the custom exception. It supports java.util.Formatter place holders,
+     * intercepted method arguments will be used as message format arguments.
      *
      * @return a custom error message when throwing the custom exception.
      * @see java.util.Formatter#format(String, Object...)
