2ea7d945f92e8d6ba7949eb4f6beb9f35702f60b
Simone Tripodi <simonetripodi@apache.org>
2013-02-23 16:16:39 +0000

    [ONAMI-86] #comment SPI modules splitted in submodules #resolve
    git-svn-id: https://svn.apache.org/repos/asf/incubator/onami/trunk/spi@1449350 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/apache/onami/spi/core/AbstractServiceClassIterator.java
@@ -0,0 +1,147 @@
+package org.apache.onami.spi.core;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import static java.lang.String.format;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import com.google.inject.ProvisionException;
+
+/**
+ * Lazy-loading iterator over the discovered classes.
+ *
+ * @param <S> The service type being loaded.
+ */
+abstract class AbstractServiceClassIterator<S>
+    implements Iterator<Class<? extends S>>
+{
+
+    /**
+     * The class or interface representing the service being loaded.
+     */
+    private final Class<S> service;
+
+    /**
+     * The class loader used to locate, load, and instantiate providers.
+     */
+    private final ClassLoader classLoader;
+
+    /**
+     * The pending providers Class names.
+     */
+    private Iterator<String> pending = null;
+
+    /**
+     * The reference to the next provider Class name.
+     */
+    private String nextName = null;
+
+    /**
+     * Creates a new Provider classes Iterator.
+     *
+     * @param service The Service being loaded.
+     * @param classLoader The ClassLoader used to load Provider classes.
+     */
+    public AbstractServiceClassIterator( Class<S> service, ClassLoader classLoader )
+    {
+        this.service = service;
+        this.classLoader = classLoader;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public final boolean hasNext()
+    {
+        if ( nextName != null )
+        {
+            return true;
+        }
+
+        while ( ( pending == null ) || !pending.hasNext() )
+        {
+            if ( !hasMorePendingNames() )
+            {
+                return false;
+            }
+            pending = getPendingNames();
+        }
+
+        nextName = pending.next();
+        return true;
+    }
+
+    /**
+     * Checks if there are still providers names to be loaded.
+     *
+     * @return true if there are still providers names to be loaded, false otherwise.
+     */
+    protected abstract boolean hasMorePendingNames();
+
+    /**
+     * Returns the iterator over next pending Providers names.
+     *
+     * @return the iterator over next pending Providers names.
+     */
+    protected abstract Iterator<String> getPendingNames();
+
+    /**
+     * {@inheritDoc}
+     */
+    public final Class<? extends S> next()
+    {
+        if ( !hasNext() )
+        {
+            throw new NoSuchElementException();
+        }
+        String className = nextName;
+        nextName = null;
+        try
+        {
+            Class<?> clazz = classLoader.loadClass( className );
+            if ( !service.isAssignableFrom( clazz ) )
+            {
+                throw new ProvisionException( format( "Provider '%s' is not assignable to Service '%s'",
+                                                      className, service.getName() ) );
+            }
+            return clazz.asSubclass( service );
+        }
+        catch ( ClassNotFoundException e )
+        {
+            throw new ProvisionException( format( "Provider '%s' not found: %s", className, e.getMessage() ) );
+        }
+        catch ( ClassCastException e )
+        {
+            throw new ProvisionException( format( "Provider '%s' is not assignable to Service '%s'",
+                                                  className, service.getName() ) );
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public final void remove()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ services/src/test/java/org/apache/onami/spi/services/FromSystemPropertiesTestCase.java
@@ -0,0 +1,82 @@
+package org.apache.onami.spi.services;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.onami.test.OnamiRunner;
+import org.apache.onami.test.annotation.GuiceProvidedModules;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.inject.Inject;
+import com.google.inject.Module;
+
+@RunWith( OnamiRunner.class )
+public final class FromSystemPropertiesTestCase
+{
+
+    @GuiceProvidedModules
+    public static Module createTestModule()
+    {
+        // This simulates the SPI specification via Java System Properties,
+        // equivalent to java -Dorg.apache.onami.spi.services.FooService=org.apac...
+        System.setProperty( "org.apache.onami.spi.services.FooService",
+                            "org.apache.onami.spi.services.FooServiceImpl1," +
+                            "org.apache.onami.spi.services.FooServiceImpl2");
+
+        return new ServiceLoaderModule()
+        {
+
+            @Override
+            protected void configureServices()
+            {
+                discover( FooService.class );
+            }
+
+        };
+    }
+
+    @Inject
+    @BarBindingAnnotation( 1 )
+    private FooService fooService1;
+
+    @Inject
+    @BarBindingAnnotation( 2 )
+    private FooService fooService2;
+
+    public void setFooService1( FooService fooService1 )
+    {
+        this.fooService1 = fooService1;
+    }
+
+    public void setFooService2( FooService fooService2 )
+    {
+        this.fooService2 = fooService2;
+    }
+
+    @Test
+    public void injectedServicesCaughtFromSystemProperties()
+    {
+        assertEquals( FooServiceImpl1.class, fooService1.getClass() );
+        assertEquals( FooServiceImpl2.class, fooService2.getClass() );
+    }
+
+}
