1d8775398bdcf11c1a1fb9334253d461cc7d51ea
ash2k <ash2k@unknown>
2014-06-29 05:52:06 +0000

    ONAMI-102 Support for JSR-250
    git-svn-id: https://svn.apache.org/repos/asf/onami/trunk@1606431 13f79535-47bb-0310-9956-ffa450edef68

---
+++ lifecycle/jsr250/src/main/java/org/apache/onami/lifecycle/jsr250/PreDestroyModule.java
@@ -0,0 +1,53 @@
+package org.apache.onami.lifecycle.jsr250;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.onami.lifecycle.core.DefaultStager;
+import org.apache.onami.lifecycle.core.LifeCycleStageModule;
+import org.apache.onami.lifecycle.core.Stager;
+
+import javax.annotation.PreDestroy;
+
+/**
+ * Guice module to register methods to be invoked when {@link org.apache.onami.lifecycle.core.Stager#stage()} is invoked.
+ * <p/>
+ * Module instance have state so it must not be used to construct more than one {@link com.google.inject.Injector}.
+ *
+ * @author Mikhail Mazursky
+ */
+public class PreDestroyModule
+    extends LifeCycleStageModule
+{
+
+    private final Stager<PreDestroy> stager = new DefaultStager<PreDestroy>(
+        PreDestroy.class, DefaultStager.Order.FIRST_IN_LAST_OUT );
+
+    @Override
+    protected void configureBindings()
+    {
+        bindStager( stager );
+    }
+
+    public Stager<PreDestroy> getStager()
+    {
+        return stager;
+    }
+
+}
